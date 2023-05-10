3d6a5b25601509569bfe4602c6c3be1f5cde7be0
Lukroza <lukroza1@gmail.com>
2023-05-10 12:28:05 +0300

    Updated the About Me Section

--- homeFunctions.js
+++ homeFunctions.js
@@ -1,3 +1,7 @@
+window.onbeforeunload = function () {
+  window.scrollTo(0, 0);
+}
+
 //Scrolling animation--------------------------
 // Create a new IntersectionObserver that takes a callback function as its argument
 const observer = new IntersectionObserver((entries) => {
@@ -59,16 +63,16 @@ document.addEventListener('mousemove', (event) => {
   dots.push(dot);
   
 });
-const elem = window.getComputedStyle(document.querySelector('.test'), ':hover').getPropertyValue('color')
-document.addEventListener('click', (event) => {
-    var text = `1px 1px 1px #919191, 1px 1px 1px #919191, 1px 2px 1px #919191,
-    1px 3px 1px #919191, 1px 4px 1px #919191, 1px 5px 1px #919191,
-    1px 6px 1px #919191, 1px 7px 1px #919191, 1px 8px 1px #919191,
-    1px 9px 1px #919191, 1px 10px 1px #919191, 1px 11px 1px #919191,
-    1px 15px 1px #919191, 1px 18px 6px rgba(16, 16, 16, 0.4),
-    1px 22px 10px rgba(16, 16, 16, 0.2), 1px 25px 35px rgba(16, 16, 16, 0.2),
-    1px 30px 60px rgba(16, 16, 16, 0.4);`;
-    elem = "#919191"
-  });
+// const elem = window.getComputedStyle(document.querySelector('.test'), ':hover').getPropertyValue('color')
+// document.addEventListener('click', (event) => {
+//     var text = `1px 1px 1px #919191, 1px 1px 1px #919191, 1px 2px 1px #919191,
+//     1px 3px 1px #919191, 1px 4px 1px #919191, 1px 5px 1px #919191,
+//     1px 6px 1px #919191, 1px 7px 1px #919191, 1px 8px 1px #919191,
+//     1px 9px 1px #919191, 1px 10px 1px #919191, 1px 11px 1px #919191,
+//     1px 15px 1px #919191, 1px 18px 6px rgba(16, 16, 16, 0.4),
+//     1px 22px 10px rgba(16, 16, 16, 0.2), 1px 25px 35px rgba(16, 16, 16, 0.2),
+//     1px 30px 60px rgba(16, 16, 16, 0.4);`;
+//     elem = "#919191"
+//   });
 
   
