c18eba0be01d1d25ee3603475d7eb856cdf0f9c9
Lukroza <lukroza1@gmail.com>
2023-05-14 23:07:02 +0300

    Added Projects Section an added JS

--- homeFunctions.js
+++ homeFunctions.js
@@ -11,6 +11,7 @@ const observer = new IntersectionObserver((entries) => {
     if(entry.isIntersecting){
       // If the entry is intersecting, add the 'show' class to the entry's target element
       entry.target.classList.add('show');
+      setTimeout(typeWrite, 2500);
     }else{
       // If the entry is not intersecting, remove the 'show' class from the entry's target element
       entry.target.classList.remove('show');
@@ -63,6 +64,33 @@ document.addEventListener('mousemove', (event) => {
   dots.push(dot);
   
 });
+
+var i =0;
+var text = `I'm currently a student at Kaunas University of Technology, where I'm studying software development. <br>
+It's a field that I'm really passionate about.
+
+I'm always eager to learn more about programming, software engineering, and related topics.
+Currently, I'm mostly working with C# using the ASP.NET framework.
+I'm also spending a lot of my free time learning about Web Development.<br> It's
+an area that I'm really interested in, and I'm eager to gain more knowledge.`
+var target = document.getElementById("cons").innerHTML;
+
+function typeWait(){
+  setTimeout(target += "/", 1000);
+  setTimeout(target.substring(0, target.length-1), 1100);
+}
+function typeWrite(){
+  if(i < text.length){
+    
+    document.getElementById("cons").innerHTML += text[i];
+    i++;
+    setTimeout(typeWrite, 40);
+    if(i == 20){}
+  }
+}
+function typeDelete(){
+  document.getElementById("cons").innerHTML = text + text[i].replace('_');
+}
 // const elem = window.getComputedStyle(document.querySelector('.test'), ':hover').getPropertyValue('color')
 // document.addEventListener('click', (event) => {
 //     var text = `1px 1px 1px #919191, 1px 1px 1px #919191, 1px 2px 1px #919191,
